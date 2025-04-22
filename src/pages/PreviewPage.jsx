cf43c5c87f4e259fa41fff1b86964ffb03db407c
a.rivera <a.riveraa99@gmail.com>
2025-04-22 12:57:21 -0500

    Implemented spoonacular api call

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -1,11 +1,66 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PreviewCard from "./PreviewCard";
 import { GuestContext } from "../components/contexts/GuestContext";
+import { RestrictionsContext } from "../components/contexts/RestrictionsContext";
+
 const PreviewPage = () => {
   const { courses, numOfDishes } = useContext(GuestContext);
+  const { dietRestrictions, foodRestrictions } =
+    useContext(RestrictionsContext);
+
+  const apiKey = import.meta.env.VITE_APP_SPOONACULAR_KEY;
+
+  // filter out unused categories
+  const filteredCourses = courses.filter((course) => numOfDishes[course] != 0);
   const [isLoading, setIsLoading] = useState(true);
-  // filter out the courses that aren't wanted
-  // making the course sections
+  const [dishes, setDishes] = useState([]);
+  const [assigning, setAssigning] = useState([]);
+
+  useEffect(() => {
+    // * fetch dishes including restrictions
+    const fetchDishes = async (course) => {
+      let specificCourse = "";
+      // spoonacular doesn't use the term entrees
+      if (course != "Entrees") specificCourse = course.toLowerCase();
+      else specificCourse = "main course";
+
+      const num = numOfDishes[course];
+
+      // restrictions urls
+      const dietUrl =
+        dietRestrictions && dietRestrictions.length > 0
+          ? `diet=${dietRestrictions.toString()}`
+          : "";
+      const foodUrl =
+        foodRestrictions && foodRestrictions.length > 0
+          ? `excludeIngredients=${foodRestrictions.toString()}`
+          : "";
+      const mainUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&type=${specificCourse}&number=${num}${
+        dietRestrictions && dietRestrictions.length > 0 ? `&${dietUrl}` : ""
+      }${foodRestrictions && foodRestrictions.length > 0 ? `&${foodUrl}` : ""}`;
+
+      const response = await fetch(mainUrl);
+      const data = await response.json();
+
+      let result = data.results.map((dish) => ({
+        title: dish.title,
+        sourceUrl: dish.sourceUrl,
+        image: dish.image,
+        course: course,
+      }));
+      return result;
+    };
+    // * fetch for each course that isn't 0
+    const fetchAll = async () => {
+      const fetchCourses = filteredCourses.map((course) => fetchDishes(course));
+      const dishes = await Promise.all(fetchCourses);
+      setDishes(dishes.flat());
+      setIsLoading(false);
+    };
+
+    fetchAll();
+  }, []);
+  console.log(dishes);
   return (
     <>
       {isLoading ? (
@@ -19,7 +74,7 @@ const PreviewPage = () => {
             Courses
           </h2>
           <div className="overflow-y-scroll  w-full h-8/10">
-            {courses.map((course) => (
+            {filteredCourses.map((course) => (
               <div className="w-full h-fit ">
                 <div className="w-full h-fit mb-2">
                   <h2 className="text-center text-xl font-bold py-2">
